49560a924514e9089b0c1baa3c01510715f93735
Nasim-Mahmud <nasim.mishu@northsouth.edu>
2020-12-06 18:33:47 +0600

    Speech to text recognization enabled

--- SpeechToText.py
+++ SpeechToText.py
@@ -1,3 +1,7 @@
+# People on Windows may face problem with pyaudio I faced the same
+# pip install pipwin
+# pipwin install pyaudio
+
 import pyttsx3 as pt
 from pyttsx3 import voice
 import speech_recognition as sr
@@ -16,21 +20,3 @@ with sr.Microphone() as source:
     except:
         print('Sorry, not clear')
 
-engine = pt.init()
-voices = engine.getProperty('voices')
-
-if(text == 'hello'):
-    engine.setProperty('voice', voices[1].id)
-    engine.say("Hello sweety!")
-    
-
-elif(text == 'how are you'):
-    engine.setProperty('voice', voices[0].id)
-    engine.say("Awesome buddy, thanks")
-
-engine.runAndWait()
-
-
-# People on Windows may face problem with pyaudio I faced the same
-# pip install pipwin
-# pipwin install pyaudio
