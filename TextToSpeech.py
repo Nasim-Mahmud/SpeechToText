400a16ca872ec5f30345f4f1af5fc51d290a359d
Nasim-Mahmud <nasim.mishu@northsouth.edu>
2020-12-06 18:30:47 +0600

    Updated

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -3,8 +3,11 @@ from pyttsx3 import voice
 engine = pt.init()
 voices = engine.getProperty('voices')
 
-engine.setProperty('voice', voices[1].id)
+# Changing the gender of voice to female
+engine.setProperty('voice', voices[1].id) 
 engine.say("Hello, My name is Sara, and you?")
+
+# Changing the gender of voice to male
 engine.setProperty('voice', voices[0].id)
 engine.say("Hey, I am Pirate, nice to meet you Sara")
 
